3d0a63042adb9c276a3c7aa6a48cf3df12d5c523
YevgenyBen <kain425@gmail.com>
2020-03-23 15:54:35 +0200

    small changes on auth

--- controllers/tokenController.js
+++ controllers/tokenController.js
@@ -1,10 +1,10 @@
 var jwt = require("jsonwebtoken");
 
 exports.verifyToken = (req, res, next) => {
-  const bearerHeader = req.headers["authorization"];
+  const bearerHeader = req.headers["header"];
   if (typeof bearerHeader !== "undefined") {
     const bearer = bearerHeader.split(" ");
-    const bearerToken = bearer[1];
+    const bearerToken = bearer[2];
     req.token = bearerToken;
 
     jwt.verify(bearerToken, "secretkey", (err, authData) => {
